6a57ccf754b3880c01e3fad4d41b4b7d00eceba9
amokan <amokan@tenstorrent.com>
2026-09-21 15:31:07 +0000

    Refactor math hw config

--- tt_metal/tt-llk/tests/sources/quasar/eltwise_binary_broadcast_quasar_test.cpp
+++ tt_metal/tt-llk/tests/sources/quasar/eltwise_binary_broadcast_quasar_test.cpp
@@ -44,9 +44,7 @@ void run_kernel(RUNTIME_PARAMETERS params)
         _llk_unpack_configure_binary_<p_unpacr::UNP_A, p_unpacr::UNP_B>(
             static_cast<DataFormat>(formats.unpack_A_dst), static_cast<DataFormat>(formats.unpack_B_dst));
         _llk_unpack_binary_broadcast_operands_init_<BROADCAST_TYPE>(
-            ckernel::trisc::bfd_current<ckernel::trisc::BfdResource::Unp0>(),
-            ckernel::trisc::bfd_current<ckernel::trisc::BfdResource::Unp1>(),
-            TILE_CNT);
+            ckernel::trisc::bfd_current<ckernel::trisc::BfdResource::Unp0>(), ckernel::trisc::bfd_current<ckernel::trisc::BfdResource::Unp1>(), TILE_CNT);
         PROFILER_SYNC();
     }
     {
@@ -109,21 +107,7 @@ void run_kernel(RUNTIME_PARAMETERS params)
         }
 
         DataFormat math_format = static_cast<DataFormat>(formats.math);
-        if constexpr (is_fp32_dest_acc_en)
-        {
-            if (static_cast<DataFormat>(formats.pack_src) == DataFormat::Int32)
-            {
-                _llk_math_srcAB_hw_configure_<IMPLIED_MATH_FORMAT, false /*fp32_dest*/, true /*int32_dest*/>(math_format, math_format);
-            }
-            else
-            {
-                _llk_math_srcAB_hw_configure_<IMPLIED_MATH_FORMAT, true /*fp32_dest*/, false /*int32_dest*/>(math_format, math_format);
-            }
-        }
-        else
-        {
-            _llk_math_srcAB_hw_configure_<IMPLIED_MATH_FORMAT, false /*fp32_dest*/, false /*int32_dest*/>(math_format, math_format);
-        }
+        _llk_math_srcAB_hw_configure_<IMPLIED_MATH_FORMAT, is_fp32_dest_acc_en>(math_format, math_format);
 
         _llk_math_eltwise_binary_broadcast_init_<ELTWISE_BINARY_OP, BROADCAST_TYPE, MATH_FIDELITY>(DEFAULT_TENSOR_SHAPE);
         PROFILER_SYNC();
